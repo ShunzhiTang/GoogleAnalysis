4fc602dd2dbce03e69c083970b735905c73933ca
tangshunzhi <1046975221@qq.com>
2015-12-24 16:31:55 +0800

    init

---
+++ GoogleAnalysis/GoogleAnalysis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleAnalysis
+//
+//  Created by tang on 15/12/24.
+//  Copyright © 2015年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
